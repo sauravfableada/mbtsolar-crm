5b16d658039bbf48e07a0470c33602003dbc5626
codescy <chaudharysneh02@gmail.com>
2026-09-05 17:18:50 +0530

    pdf logo moved to left corner

--- resources/views/pdfbuilder/partials/pdf-page-header.blade.php
+++ resources/views/pdfbuilder/partials/pdf-page-header.blade.php
@@ -1,9 +1,22 @@
 <table width="100%" cellpadding="0" cellspacing="0" style="margin-bottom: 0px;">
     <tr>
         <td width="50%" align="left" valign="middle">
-            <div style="font-size: 18px; font-family: 'Montserrat', sans-serif;">
+            <?php if (!empty($_customLogoBase64ForPages)): ?>
+                        <table width="100%" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td width="175" align="left" valign="middle" style="padding-right:15px;">
+                                    <img src="<?= $_customLogoBase64ForPages ?>" alt="Custom Logo" style="max-width:160px; max-height:55px; height:auto; object-fit:contain;">
+                                </td>
+                                <td align="left" valign="middle" style="white-space:nowrap;">
+                        <?php endif; ?>
+                        <div style="font-size: 18px; font-family: 'Montserrat', sans-serif;">
                 <?= $quantity ?>kW Ongrid <?= $pdfTypeLabelMixed ?>
             </div>
+                        <?php if (!empty($_customLogoBase64ForPages)): ?>
+                                </td>
+                            </tr>
+                        </table>
+                        <?php endif; ?>
         </td>
         <td width="50%" align="right" valign="middle">
             <?php if (!empty($logoBase64)): ?>
